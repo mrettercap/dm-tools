173f7c16fcf4ad756afbf283877e531d904f25c5
mrettercap <elfordo@gmail.com>
2013-06-08 22:17:49 -0500

    Initial commit; dm-tools forked from separate project

---
+++ README.md
@@ -0,0 +1,16 @@
+# dm-tools
+
+dm-tools is a set of tools designed to aid the dungeon master of a pen and paper RPG in doing his or her job. 
+There's a more cohesive future for dm-tools, but for now it's just a loose collection of programs, some of which are rather old. 
+
+## roll.sh
+Roll is a simple die roller. Specify the number of sides to the die and how many you wish to roll, and watch the magic happen.
+
+### Syntax:
+roll NdNN
+
+### Usage:
+
+	$ roll 2d20
+	* 2d20: 13 10
+ 	< Total: 23 >
---
+++ roll.sh
@@ -0,0 +1,56 @@
+#!/bin/bash 
+
+# Dice roller for Tabletops.
+# by Benjamin Ford
+
+# Syntax: roll {sides to die} {damage modifier} 
+#     eg: roll 3d20 +4
+
+# First arg: number of sides to the die!
+DIE=$1
+
+# How do we use this damn thing?
+usage() {
+     # Rolla boy tell 'em
+     echo "Usage: $0 {roll modifier}d{number of sides to die}"
+     echo "eg. $0 3d20"
+     echo
+     exit 0
+
+}
+
+# This is our rolling function
+roll() {
+  # Starting a for loop that goes from 1 to however many dice are specified in the roll modifier
+  for index in $(eval echo {1..$1})
+  do
+     # For each number of dice, generate a random number between 1 and the number of sides to the die, then add to an array.
+     ROLLS[$index]=$[ ( $RANDOM % $2 ) +1 ]
+  done  
+  
+  # Then, unpack the array. 
+  for rnum in "${ROLLS[@]}"
+  do
+     # We want to make our overall roll the sum of each of the rolls.
+     let ROLL=$[ $ROLL + $rnum ]
+  done
+
+  # Print the number of sides to the die as well as the dice modifier.
+  printf "* \033[0;32m${MF}\033[0;31md$DIE\033[1;34m: "
+
+  # If we're rolling multiple dice, print the individual rolls along with the total. Otherwise, just print the total.
+  [[ "$1" -gt 1 ]] && echo -e "\033[1;36m${ROLLS[@]} \n\033[0;30m\033[42m < Total: $ROLL > \033[00m" || echo -e "\033[1;36m$ROLL\033[1;34m."
+
+  echo
+
+}
+
+# Are we rolling in the right format?
+case $DIE in
+     # If we receive input in the format of ./roll NxN, ./roll NNxN, or ./roll NxNN, break up the roll modifier and test for damage modifiers.
+     [1-9]d[0-9]|[1-9][0-9]d[0-9]|[1-9]d[0-9][0-9]|[1-9]d[0-9][0-9][0-9]|[1-9][0-9]d[0-9][0-9]|[1-9][0-9]d[0-9][0-9][0-9]) MF=${DIE%d*}; DIE=${DIE##*d}; roll "$MF" "$DIE" ;;
+     d[1-9]|d[1-9][0-9]|d[1-9][0-9][0-9]) DIE=${DIE##*d}; roll 1 "$DIE" ;;
+     [1-9]|[1-9][0-9]|[1-9][0-9][0-9]) roll 1 $1 ;;
+     # If we get some other crazy input, show 'em the usage. 
+     *) usage ;;
+esac
